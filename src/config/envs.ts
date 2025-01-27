1189a063f81500171943b66f7e4f31e61084aad0
FacuBzn <juanfacundobazan@gmail.com>
2025-01-27 08:57:06 -0300

    Add PaginationDto for pagination configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,12 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  DATABASE_URL: string;
 }
 
 const envVarsSchema = joi.object({
   PORT: joi.number().required().default(3000),
+  DATABASE_URL: joi.string().required(),
 })
 .unknown(true);
 
@@ -22,4 +24,5 @@ const envVars: EnvVars = value;
 
 export const envs =  {
   port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
 }
