b3766be0269e7573ea94f5576c6804b174ba14ce
FacuBzn <juanfacundobazan@gmail.com>
2025-01-27 09:44:53 -0300

    Refactor code to implement error handling in ProductsService findOne method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -40,8 +40,21 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    if (!id) {
+      throw new NotFoundException('Product ID is required');
+    }
+    if (typeof id !== 'number') {
+      throw new NotFoundException('Invalid Product ID, must be a number');
+    }
+
+    const product = await this.product.findUnique({
+      where: { id }
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found.`);
+    }
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
