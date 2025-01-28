110ee3d75e99f20ca723012db8496c1de24a65d1
FacuBzn <juanfacundobazan@gmail.com>
2025-01-27 21:20:25 -0300

    converting my resfull api into a microservice, configuration needed in my main

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const logger = new Logger('ProductsMS-Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,{
+    transport: Transport.TCP,
+    options: {
+      /* host: envs.host, */
+      port: envs.port
+    }
+    }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +25,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.port);
-  logger.log(`Application is running on port: ${envs.port}`);
+  await app.listen();
+  logger.log(`Products Microservice running on port: ${envs.port}`);
 }
 bootstrap();
